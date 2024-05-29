f5b634bc7b8d37cdf2400d050aad80a9c7e6f2fd
Sol Leiva <sol.leiva@istea.com.ar>
2024-05-29 14:54:05 -0300

    Coleccionable
    Agregue que los coleccionables se destruyan con el enemigo tambien

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -7,7 +7,7 @@ public class Items : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player") || other.CompareTag("Enemy"))
         {
            Destroy(gameObject);
         }    
