b67120d276d40d8d69b3012225dbe993b26af08e
alejobalboa <alejo.balboa@yahoo.com.ar>
2024-06-11 15:55:14 -0300

    Modificacion camara

---
+++ Assets/Scripts/CameraFP.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFP : MonoBehaviour
+{
+    public Transform playerRef;
+    [SerializeField]public float mouseSensitivity = 5f;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); /*Sirve para que la cámara no pueda pasar de los 90 grados para arriba o para abajo*/
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerRef.Rotate(Vector3.up * mouseX);
+    }
+}
