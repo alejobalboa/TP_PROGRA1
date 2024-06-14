65e6806ad9cbeea4c808877fbdbde9375509d755
Sol Leiva <sol.leiva@istea.com.ar>
2024-06-14 16:26:22 -0300

    Remy 1ra persona
    Remy primera persona + NavMesh

--- Assets/Scripts/Remy_New.cs
+++ Assets/Scripts/Remy_New.cs
@@ -5,20 +5,17 @@ using Cinemachine;
 
 public class Remy_New : MonoBehaviour
 {
-
     [SerializeField] private Animator animator;
     [SerializeField] private GameObject CinemachineCameraTarget;
     [SerializeField] private LayerMask AimColliderLayerMask;
     [SerializeField] private GameObject _mainCamera;
 
-    private float _rotationVelocity;
-    private float _targetRotation = 0.0f;
+    private float rotationVelocity;
+    private float targetRotation = 0.0f;
     private bool rotateOnMove = true;
-    private float _cinemachineTargetYaw;
-    private float _cinemachineTargetPitch;
-    private float sensitivity;
-    private float normalSensitivity = 1f;
-    private float aimSensitivity = 0.5f;
+    private float cinemachineTargetYaw;
+    private float cinemachineTargetPitch;
+    private float sensitivity = 1f;
     private float TopClamp = 70.0f;
     private float BottomClamp = -30.0f;
     private float mousex;
@@ -29,30 +26,43 @@ public class Remy_New : MonoBehaviour
 
     private Vector3 mouseWorldPosition;
 
-    public void Awake()
+    public void Start()
     {
-        _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
+        cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
     }
 
     public void Update()
     {
-
-        float targetSpeed = 5f;
-        if (horizontal == 0 && vertical == 0) targetSpeed = 0.0f;
-
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
         mousex = Input.GetAxis("Mouse X");
         mousey = Input.GetAxis("Mouse Y");
 
-        var direction = new Vector3(horizontal, 0, vertical).normalized;
-
+        float targetSpeed = (horizontal == 0 && vertical == 0) ? 0.0f : 5f;
         animator.SetFloat("speed", targetSpeed);
 
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
-        mouseWorldPosition = Vector3.zero;
+        UpdateMouseWorldPosition();
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            RotateCharacter(direction);
+        }
+
+        MoveCharacter(targetSpeed);
+    }
+
+    private void LateUpdate()
+    {
+        CameraRotation();
+        RotateTowardsMouseWorldPosition();
+    }
+
+
+    private void UpdateMouseWorldPosition()
+    {
         Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
-
         Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
 
         if (Physics.Raycast(ray, out RaycastHit raycastHit, 200f, AimColliderLayerMask))
@@ -63,29 +73,27 @@ public class Remy_New : MonoBehaviour
         {
             mouseWorldPosition = Camera.main.transform.position + Camera.main.transform.forward * 200f;
         }
+    }
 
-       
+    private void RotateCharacter(Vector3 direction)
+    {
+        targetRotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _mainCamera.transform.eulerAngles.y;
+        float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity, RotationSmoothTime);
 
-        if (horizontal != 0 || vertical != 0)
+        if (rotateOnMove)
         {
-            _targetRotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _mainCamera.transform.eulerAngles.y;
-
-            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, RotationSmoothTime);
-
-            if (rotateOnMove)
-            {
-                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
-            }
+            transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
         }
+    }
 
-        Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
-
+    private void MoveCharacter(float targetSpeed)
+    {
+        Vector3 targetDirection = Quaternion.Euler(0.0f, targetRotation, 0.0f) * Vector3.forward;
         transform.position += targetDirection.normalized * (targetSpeed * Time.deltaTime);
     }
 
-    private void LateUpdate()
+    private void RotateTowardsMouseWorldPosition()
     {
-        CameraRotation();
         Vector3 worldTarget = mouseWorldPosition;
         worldTarget.y = transform.position.y;
         Vector3 rotateDirection = (worldTarget - transform.position).normalized;
@@ -96,24 +104,25 @@ public class Remy_New : MonoBehaviour
     {
         if (mousex != 0 || mousey != 0)
         {
-
-            _cinemachineTargetYaw += mousex * sensitivity;
-            _cinemachineTargetPitch += mousey * sensitivity * (-1);
+            rotateOnMove = false;
+            cinemachineTargetYaw += mousex * sensitivity;
+            cinemachineTargetPitch += mousey * sensitivity * (-1);
+        }
+        else
+        {
+            rotateOnMove = true;
         }
 
-        _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
-        _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+        cinemachineTargetYaw = ClampAngle(cinemachineTargetYaw, float.MinValue, float.MaxValue);
+        cinemachineTargetPitch = ClampAngle(cinemachineTargetPitch, BottomClamp, TopClamp);
 
-        CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch, _cinemachineTargetYaw, 0.0f);
-
+        CinemachineCameraTarget.transform.rotation = Quaternion.Euler(cinemachineTargetPitch, cinemachineTargetYaw, 0.0f);
     }
 
-    private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+    private static float ClampAngle(float angle, float min, float max)
     {
-        if (lfAngle < -360f) lfAngle += 360f;
-        if (lfAngle > 360f) lfAngle -= 360f;
-        return Mathf.Clamp(lfAngle, lfMin, lfMax);
+        if (angle < -360f) angle += 360f;
+        if (angle > 360f) angle -= 360f;
+        return Mathf.Clamp(angle, min, max);
     }
-
-
 }
